575a274cc91c10871f1e520ba6f0c353b1749794
Wahidullah Shadab <shadabwahidullah@gmail.com>
2021-10-22 12:04:54 +0430

    Add tests for counting comments

--- src/test/comments.test.js
+++
@@ -1,7 +0,0 @@
-import { countComments } from "../comments.js";
-
-describe("Testing comments counter", () => {
-  test("First test", () => {
-    expect(countComments([{}, {}, {}])).toBe(3);
-  });
-});
